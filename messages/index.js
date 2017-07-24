8f10dfe7eff4081879cf3f5d6a6374787a0abf0e
Aaron Chong <aaronchongkh@gmail.com>
2017-07-25 00:45:04 +0800

    aaronbot_MicrosoftAzure

---
+++ messages/index.js
@@ -0,0 +1,41 @@
+// For more information about this template visit http://aka.ms/azurebots-node-qnamaker
+
+"use strict";
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
+var builder_cognitiveservices = require("botbuilder-cognitiveservices");
+
+var useEmulator = (process.env.NODE_ENV == 'development');
+
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    stateEndpoint: process.env['BotStateEndpoint'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+});
+
+var bot = new builder.UniversalBot(connector);
+
+var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
+                knowledgeBaseId: process.env.QnAKnowledgebaseId, 
+    subscriptionKey: process.env.QnASubscriptionKey});
+
+var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
+    recognizers: [recognizer],
+                defaultMessage: 'No match! Try changing the query terms!',
+                qnaThreshold: 0.3}
+);
+
+
+bot.dialog('/', basicQnAMakerDialog);
+
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpoint at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());    
+} else {
+    module.exports = { default: connector.listen() }
+}
